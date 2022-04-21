5706e7506adf7e1be28aa997d46bf0308a5f6aa8
atikanc <atikanchokpapone@gmail.com>
2022-04-21 15:14:02 -0500

    Reset DB Every Deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:reset
-bundle exec rake db:create
-bundle exec rake db:migrate
-bundle exec rake db:seed
+#bundle exec rake db:create
+#bundle exec rake db:migrate
+#bundle exec rake db:seed
